5c9b85c7f0eebb52810b2278be9c6c8813a29b3e
Akshaya C K <akshayack21>
2025-02-19 00:15:57 +0530

    full stack food delivery website using MERN

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,19 @@
+// assets.js
+import tavolo_logo from './tavolo_logo.png';
+import add_icon from './add_icon.png';
+import order_icon from './order_icon.png';
+import profile_image from './profile_image.png';
+import upload_area from './upload_area.png';
+import parcel_icon from './parcel_icon.png';
+
+export const url = 'http://localhost:4000';
+export const currency = '₹';
+
+export const assets = {
+    tavolo_logo,  // Changed from logo to tavolo_logo
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+};
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Pick your perfect meal from our delicious selection</h2>
+                
+               
+            </div>
+        </div>
+    )
+}
+
+export default Header
